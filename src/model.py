99a6a0986dfd12086bbb194e7189ce2a5669b238
Vishnu Murali <86015526+VishnuMuraliKr@users.noreply.github.com>
2024-12-02 14:36:57 +0530

    Delete src directory

--- src/model.py
+++
@@ -1,6 +0,0 @@
-from transformers import CLIPProcessor, CLIPModel
-
-def load_clip_model():
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-    return model, processor
